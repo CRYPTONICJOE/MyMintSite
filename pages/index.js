69808e4e4e5c178c2c1aea0e52e41f8c12ca555e
CRYPTONICJOE <110938868+CRYPTONICJOE@users.noreply.github.com>
2023-03-22 10:52:48 +0000

    title

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to me!" />
+        <Header title="Welcome to our Minting Site!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
