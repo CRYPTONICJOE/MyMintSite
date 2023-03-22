1167fdc11e3b900b3e31a1915fed4a15bee3c9dc
CRYPTONICJOE <110938868+CRYPTONICJOE@users.noreply.github.com>
2023-03-22 11:32:16 +0000

    basic

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,27 @@
-const Web3 = require('web3');
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
-// Replace these values with your own
-const contractAddress = '0x1234567890123456789012345678901234567890';
-const contractABI = [
-  {
-    "inputs": [],
-    "name": "mint",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }
-];
-const infuraKey = 'YOUR_INFURA_API_KEY';
-const network = 'mainnet';
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-async function main() {
-  // Connect to Infura node
-  const web3 = new Web3(`https://${network}.infura.io/v3/${infuraKey}`);
+      <main>
+        <Header title="Welcome to our Minting Site!" />
+        <p className="description">
+          Here you can mint our NFT collection!
+        </p>
 
-  // Check if MetaMask is installed
-  if (!window.ethereum) {
-    alert('Please install MetaMask to use this dApp');
-    return;
-  }
+        <p className="description">
+          Testing Description 2
+        </p>
+      </main>
 
-  // Request user's permission to connect to their wallet
-  await window.ethereum.enable();
-
-  // Create a contract instance
-  const contract = new web3.eth.Contract(contractABI, contractAddress);
-
-  // Listen for form submission
-  const form = document.getElementById('mint-form');
-  form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-
-    // Get the form data
-    const formData = new FormData(event.target);
-    const tokenId = formData.get('tokenId');
-
-    // Call the smart contract's "mint" function to mint the NFT
-    try {
-      const result = await contract.methods.mint(tokenId).send({from: window.ethereum.selectedAddress});
-      console.log('Transaction hash:', result.transactionHash);
-      alert('NFT minted successfully');
-    } catch (error) {
-      console.error(error);
-      alert('Failed to mint NFT');
-    }
-  });
+      <Footer />
+    </div>
+  )
 }
-
-main();
